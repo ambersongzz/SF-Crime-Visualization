0050ac80a72974c818211f3d8aca33f734e7918d
songzizhen <SONGZIZHEN@songzizhendeMacBook-Pro.local>
2018-04-26 12:45:15 -0700

    okay

--- docs/main.js
+++ docs/main.js
@@ -1,8 +1,8 @@
 $(function(){
-	$(".dropdown-menu").on("shown.bs.dropdown", function(e){
+	$("#Violent").on("click", function(e){
 	//do something
-	console.log("You clicked the drop downs", event);
-		$("#crime_choropleth").load("violent.html");
+		console.log("You clicked the drop downs", event);
+		$("#crime_choropleth").html("violent.html");
 		e.preventDefault();
 	});
 })
